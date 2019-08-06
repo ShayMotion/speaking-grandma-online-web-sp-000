411f1b0cbe89319af79b7af0c651efef3769d275
Shayan Mohsin <shaymotionfilms@gmail.com>
2019-08-06 16:43:10 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,8 +2,19 @@
 def speak_to_grandma(phrase)
   if 'Hi Nana, how are you?".downcase
   puts "HUH?! SPEAK UP, SONNY!"
+elsif 'I LOVE YOU GRANDMA!"', she should respond with"
+# 'I LOVE YOU TOO PUMPKIN!'
 else 
 "WHAT DID YOU EAT TODAY?".upcase
+puts "NO, NOT SINCE 1938!"
+
+
+
+
+
+
+
+
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
@@ -11,7 +22,7 @@ else
 # If you shout, she can hear you (or at least she thinks so) 
 # and yells back
 
-# NO, NOT SINCE 1938!
+# 
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
